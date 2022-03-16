0a190bd36bc9cfe5f3c23b4121535dc422742fe4
MitchHome <mitchherreman@gmail.com>
2022-03-16 09:54:55 +0100

    afgewerkte oefening

--- bladsteenschaar/bladsteenschaar.js
+++ bladsteenschaar/bladsteenschaar.js
@@ -1,108 +1,44 @@
 "use strict";
-// Keuze blad
-function blad () {
-    let keuzeSpeler = "blad";
-    let keuzeComputer = Math.floor((Math.random() * 3) + 1);
-    if (keuzeComputer === 1) {
-        const img = document.getElementById("bladsteenschaar");
-        img.title = "blad";
-        img.src = `blad.png`;
-        let keuzeComputer = "blad";
-        console.log("Speler", keuzeSpeler);
-        console.log("Computer", keuzeComputer);
-    } else if (keuzeComputer === 2) {
-        const img = document.getElementById("bladsteenschaar");
-        img.title = "steen";
-        img.src = `steen.png`;
-        let keuzeComputer = "steen";
-        console.log("Speler", keuzeSpeler);
-        console.log("Computer", keuzeComputer);
-    } else {
-        const img = document.getElementById("bladsteenschaar");
-        img.title = "schaar";
-        img.src = `schaar.png`;
-        let keuzeComputer = "schaar";
-        console.log("Speler", keuzeSpeler);
-        console.log("Computer", keuzeComputer);
-    }
-}
+const keuzes = ["blad", "steen", "schaar"]
 
-// Keuze steen
-function steen() {
-    let keuzeSpeler = "steen";
-    let keuzeComputer = Math.floor((Math.random() * 3) + 1);
-    if (keuzeComputer === 1) {
-        const img = document.getElementById("bladsteenschaar");
-        img.title = "blad";
-        img.src = `blad.png`;
-        let keuzeComputer = "blad";
-        console.log("Speler", keuzeSpeler);
-        console.log("Computer", keuzeComputer);
-    } else if (keuzeComputer === 2) {
-        const img = document.getElementById("bladsteenschaar");
-        img.title = "steen";
-        img.src = `steen.png`;
-        let keuzeComputer = "steen";
-        console.log("Speler", keuzeSpeler);
-        console.log("Computer", keuzeComputer);
-    } else {
-        const img = document.getElementById("bladsteenschaar");
-        img.title = "schaar";
-        img.src = `schaar.png`;
-        let keuzeComputer = "schaar";
-        console.log("Speler", keuzeSpeler);
-        console.log("Computer", keuzeComputer);
-    }
-}
-
-// Keuze schaar
-function schaar() {
-    let keuzeSpeler = "schaar";
-    let keuzeComputer = Math.floor((Math.random() * 3) + 1);
-    if (keuzeComputer === 1) {
-        const img = document.getElementById("bladsteenschaar");
-        img.title = "blad";
-        img.src = `blad.png`;
-        let keuzeComputer = "blad";
-        console.log("Speler", keuzeSpeler);
-        console.log("Computer", keuzeComputer);
-    } else if (keuzeComputer === 2) {
-        const img = document.getElementById("bladsteenschaar");
-        img.title = "steen";
-        img.src = `steen.png`;
-        let keuzeComputer = "steen";
-        console.log("Speler", keuzeSpeler);
-        console.log("Computer", keuzeComputer);
-    } else {
-        const img = document.getElementById("bladsteenschaar");
-        img.title = "schaar";
-        img.src = `schaar.png`;
-        let keuzeComputer = "schaar";
-        console.log("Speler", keuzeSpeler);
-        console.log("Computer", keuzeComputer);
-    }
+// Keuzes
+function verwerkKeuzeSpeler() {
+    verwijderselecties();
+    this.classList.add("gekozen");
+    let keuzeSpeler = this.dataset.keuze;
+    let keuzeComputer = Math.floor((Math.random() * 3));
+    keuzeComputer = keuzes[keuzeComputer];
+    const img = document.getElementById("bladsteenschaar");
+    img.title = keuzeComputer;
+    img.src = `${keuzeComputer}.png`;
+    document.getElementById("resultaat").innerText = resultaat(keuzeSpeler, keuzeComputer);
 }
 
 // Resultaat
-function resultaat () {
+function resultaat(keuzeSpeler, keuzeComputer) {
     if (keuzeSpeler === keuzeComputer) {
         return "Gelijkspel";
     } else if (keuzeSpeler === "blad" && keuzeComputer === "steen") {
-        return "Winnaar";
+        return "Speler";
     } else if (keuzeSpeler === "blad" && keuzeComputer === "schaar") {
-        return "Verliezer";
+        return "Computer";
     } else if (keuzeSpeler === "steen" && keuzeComputer === "blad") {
-        return "Verliezer";
+        return "Computer";
     } else if (keuzeSpeler === "steen" && keuzeComputer === "schaar") {
-        return "Winnaar";
+        return "Speler";
     } else if (keuzeSpeler === "schaar" && keuzeComputer === "blad") {
-        return "Winnaar";
+        return "Speler";
     } else if (keuzeSpeler === "schaar" && keuzeComputer === "steen") {
-        return "Verliezer";
+        return "Computer";
     }
 }
 
-document.getElementById("blad").onclick = blad;
-document.getElementById("steen").onclick = steen;
-document.getElementById("schaar").onclick = schaar;
-document.getElementById("resultaat").innerText = resultaat();
+function verwijderselecties() {
+    for (const element of document.querySelectorAll("a.gekozen")) {
+        element.classList.remove("gekozen");
+    }
+}
+
+for (const link of document.querySelectorAll("#keuzeSpeler a")) {
+    link.onclick = verwerkKeuzeSpeler;
+}
